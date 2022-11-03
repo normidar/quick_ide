440af391a382598116902f251a0daed604592282
劉達 <>
2022-11-04 00:58:27 +0900

    u

--- lib/pages/keyboard/key_button.dart
+++ lib/pages/keyboard/key_button.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:quick_ide/pages/keyboard/type_controller.dart';
 
 class KeyButton extends StatelessWidget {
-  const KeyButton({
+  KeyButton({
     super.key,
     required this.text,
     required this.typeController,
@@ -14,6 +13,25 @@ class KeyButton extends StatelessWidget {
   final TypeController typeController;
   final int flex;
 
+  bool isPressed = false;
+  int deleteSpeed = 200;
+  void onTapDown() {
+    isPressed = true;
+    deleteSpeed = 200;
+    if (text == 'bs') {
+      Future.delayed(const Duration(milliseconds: 700)).then((value) async {
+        while (isPressed) {
+          typeController.backspace();
+          await Future.delayed(Duration(milliseconds: deleteSpeed > 50 ? deleteSpeed -= 2 : deleteSpeed));
+        }
+      });
+    }
+  }
+
+  void onTapUp() {
+    isPressed = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -22,19 +40,33 @@ class KeyButton extends StatelessWidget {
         padding: const EdgeInsets.all(1.0),
         child: Material(
           color: Colors.blue.shade300,
-          child: InkWell(
-            onTap: () {
-              HapticFeedback.lightImpact();
-              if (text == 'bs') {
-                typeController.backspace();
-                return;
-              }
-              typeController.typeText(text);
-            },
-            onLongPress: () {
-              typeController.typeText(text.toUpperCase());
-            },
-            child: Center(child: Text(text)),
+          child: Listener(
+            onPointerDown: ((event) => onTapDown()),
+            onPointerUp: (event) => onTapUp(),
+            onPointerCancel: (event) => onTapUp(),
+            child: InkWell(
+              onTap: () {
+                if (text == 'bs') {
+                  typeController.backspace();
+                  return;
+                } else if (text == 'Tab') {
+                  typeController.typeText('    ');
+                  return;
+                }
+                typeController.typeText(text);
+              },
+              onLongPress: () {
+                if (text == 'bs') {
+                  typeController.backspace();
+                  return;
+                } else if (text == 'Tab') {
+                  typeController.typeText('    ');
+                  return;
+                }
+                typeController.typeText(text.toUpperCase());
+              },
+              child: Center(child: Text(text)),
+            ),
           ),
         ),
       ),
--- lib/pages/keyboard/keyboard.dart
+++ lib/pages/keyboard/keyboard.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:quick_ide/pages/keyboard/key_button.dart';
 import 'package:quick_ide/pages/keyboard/type_controller.dart';
 
-const List<String> line7 = ['+', '-', '*', '/', ':', '=', '&', '|'];
-const List<String> line6 = ['!', '"', '#', '\$', '%', "'", '~', '^', '?'];
-const List<String> line5 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'bs'];
+const List<String> line7 = ['+', '-', '*', '/', ':', '.', '=', '&', '|', 'bs'];
+const List<String> line6 = ['Tab', '!', '"', '#', '\$', '%', "'", '~', '^', '?'];
+const List<String> line5 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const List<String> line4 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
 const List<String> line3 = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\n'];
 const List<String> line2 = ['(', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ')', ','];
